e63a067144b271188c57eb78477dc36fbeb6d197
aliaksandrsurma <aliaksandr.surma@gmail.com>
2023-12-24 00:25:58 +0300

    Initial structure of Spring Boot app

---
+++ src/main/java/com/asurma/cvaap/api/CvAsAProjectApiApplication.java
@@ -0,0 +1,13 @@
+package com.asurma.cvaap.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvAsAProjectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvAsAProjectApiApplication.class, args);
+	}
+
+}
