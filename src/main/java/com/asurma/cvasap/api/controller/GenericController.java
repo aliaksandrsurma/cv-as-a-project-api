c6c615fd2a5e07934736ea3cf5fd819cb19f8529
aliaksandrsurma <aliaksandr.surma@gmail.com>
2024-07-05 11:42:13 +0300

    Fixing 'hello' endpoint and enabling initial data.

--- src/main/java/com/asurma/cvasap/api/controller/GenericController.java
+++ src/main/java/com/asurma/cvasap/api/controller/GenericController.java
@@ -2,12 +2,13 @@ package com.asurma.cvasap.api.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GenericController {
 
     @GetMapping("/hello")
-    public String getHello() {
+    public @ResponseBody String getHello() {
         return "Hello, this is a test API endpoint.";
     }
 
